a654a197f9e78bb50acf42a7263b75cb37dd9a61
Phaneendra Venkat Gatta <gphaneendra@corp.ojas-it.com>
2020-06-26 19:57:06 +0530

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
+function tree(){
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+}
 
 @Component({
   selector: 'app-home',
@@ -10,6 +22,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+   tree()
+}
 
 }
